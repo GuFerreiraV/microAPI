b14973a54696ff92d28962c8506c024314c4c167
GuFerreiraV <gustavoferreiravargens@gmail.com>
2026-06-26 10:23:14 -0300

    feat: funções de clique

--- microAPI/MainWindow.axaml.cs
+++ microAPI/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 ﻿using microAPI.ViewModels;
+using Avalonia.Interactivity;
+using Avalonia.Input;
 using Avalonia.Controls;
 
 namespace microAPI
@@ -15,6 +17,38 @@ namespace microAPI
         public MainWindow(MainViewModel viewModel) : this()
         {
             DataContext = viewModel;
+        }
+        // Inicia edição ao dar clique duplo no nome
+        private void TextBlock_DoubleTapped(object? sender, TappedEventArgs e)
+        {
+            if (sender is TextBlock textBlock && textBlock.DataContext != null)
+            {
+                dynamic vm = textBlock.DataContext;
+                vm.StartEditCommand.Execute(null);
+            }
+        }
+
+        // Salva ao perder o foco (clicar fora)
+        private void TextBox_LostFocus(object? sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox && textBox.DataContext != null)
+            {
+                dynamic vm = textBox.DataContext;
+                vm.EndEditCommand.Execute(null);
+            }
+        }
+
+        // Salva ao pressionar a tecla Enter
+        private void TextBox_KeyDown(object? sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Enter)
+            {
+                if (sender is TextBox textBox && textBox.DataContext != null)
+                {
+                    dynamic vm = textBox.DataContext;
+                    vm.EndEditCommand.Execute(null);
+                }
+            }
         }
     }
 }
